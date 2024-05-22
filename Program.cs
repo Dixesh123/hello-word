fd73d900c3ab8a6e6b60a2027d64d342c9d4c4c2
Dixesh123 <dixoza4667@gmail.com>
2024-05-22 15:22:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dikho_test1
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi students ");
+        }
+    }
+}
